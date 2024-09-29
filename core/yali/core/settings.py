31bf568163f260a45b485046edcbab6ef8454844
Ravishankar Haranath <ravikh@gmail.com>
2024-09-29 19:11:15 +0530

    Telemetry - added initial code

---
+++ core/yali/core/settings.py
@@ -0,0 +1,103 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import (
+    MongoDsn,
+    Field,
+    AliasChoices,
+    SecretStr,
+    computed_field,
+    model_validator,
+    StringConstraints,
+)
+from typing import Annotated, Literal
+
+
+class MongoDBSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_file_encoding="utf-8", case_sensitive=True)
+
+    mongo_url: MongoDsn = Field(..., validation_alias=AliasChoices("YALI_MONGO_URL", "MONGODB_URL"))
+    mongo_username: str = Field(
+        ..., validation_alias=AliasChoices("YALI_MONGO_USER", "MONGODB_USER")
+    )
+    mongo_password: SecretStr = Field(
+        ..., validation_alias=AliasChoices("YALI_MONGO_PASSWORD", "MONGODB_PASSWORD")
+    )
+    mongo_pool_size: int = Field(
+        25, gt=0, validation_alias=AliasChoices("YALI_MONGO_POOL_SIZE", "MONGODB_POOL_SIZE")
+    )
+
+
+OTelResourceAttrsStr = Annotated[
+    str,
+    StringConstraints(
+        strip_whitespace=True,
+        to_lower=True,
+        pattern=r"^service.name=[a-zA-Z0-9_.-]+,service.version=[a-zA-Z0-9_.-]+,deployment.environment=[a-zA-Z0-9_.-]+(,\w+=[a-zA-Z0-9_.,%&@\'\"\[\]-]+)*$",
+    ),
+]
+
+
+class OTelOTLPSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_file_encoding="utf-8", case_sensitive=True)
+
+    otel_exporter: Literal["otlp"] = "otlp"
+    otel_exporter_headers: str = Field(
+        ...,
+        validation_alias=AliasChoices("YALI_OTEL_EXPORTER_HEADERS", "OTEL_EXPORTER_OTLP_HEADERS"),
+    )
+    otel_exporter_endpoint: str = Field(
+        ...,
+        validation_alias=AliasChoices("YALI_OTEL_EXPORTER_ENDPOINT", "OTEL_EXPORTER_OTLP_ENDPOINT"),
+    )
+    otel_resource_attributes: OTelResourceAttrsStr = Field(
+        ...,
+        validation_alias=AliasChoices("YALI_OTEL_RESOURCE_ATTRIBUTES", "OTEL_RESOURCE_ATTRIBUTES"),
+    )
+    otel_export_interval_millis: float = Field(
+        5000,
+        ge=1000,
+        validation_alias=AliasChoices(
+            "YALI_OTEL_EXPORT_INTERVAL_MILLIS", "OTEL_EXPORT_INTERVAL_MILLIS"
+        ),
+    )
+    otel_exporter_certificate: str | None = Field(
+        None,
+        validation_alias=AliasChoices(
+            "YALI_OTEL_EXPORTER_CERTIFICATE", "OTEL_EXPORTER_OTLP_CERTIFICATE"
+        ),
+    )
+    otel_exporter_certchain: str | None = Field(
+        None,
+        validation_alias=AliasChoices(
+            "YALI_OTEL_EXPORTER_CERTCHAIN", "OTEL_EXPORTER_OTLP_CERTCHAIN"
+        ),
+    )
+    otel_exporter_privkey: str | None = Field(
+        None,
+        validation_alias=AliasChoices("YALI_OTEL_EXPORTER_PRIVKEY", "OTEL_EXPORTER_OTLP_PRIVKEY"),
+    )
+
+    @computed_field
+    @property
+    def resource_attributes(self):
+        res_attributes = {}
+        resource_pairs = self.otel_resource_attributes.split(",")
+
+        for resource_pair in resource_pairs:
+            key, value = resource_pair.split("=")
+            res_attributes[key] = value
+
+        return res_attributes
+
+    @model_validator(mode="after")
+    def check_certchain_and_privkey(self):
+        if self.otel_exporter_certchain is not None and self.otel_exporter_privkey is None:
+            raise ValueError(
+                "If 'otel_exporter_certchain' is set, 'otel_exporter_privkey' must be set as well"
+            )
+
+        if self.otel_exporter_certchain is None and self.otel_exporter_privkey is not None:
+            raise ValueError(
+                "If 'otel_exporter_privkey' is set, 'otel_exporter_certchain' must be set as well"
+            )
+
+        return self
---
+++ telemetry/yali/telemetry/__init__.py
@@ -0,0 +1,90 @@
+from opentelemetry import trace as otel_trace
+from opentelemetry.sdk.trace import TracerProvider, Tracer
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.resources import Resource
+
+from yali.core.metatypes import UniType
+from yali.core.settings import OTelOTLPSettings
+from opentelemetry import metrics as otel_metrics
+from opentelemetry.sdk.metrics import MeterProvider, Meter
+from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
+from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import OTLPMetricExporter
+from opentelemetry.sdk.resources import Resource
+
+from typing import Dict
+import grpc
+
+
+class YaliTelemetry(metaclass=UniType):
+    __settings = OTelOTLPSettings()
+
+    def __init__(self):
+        self._insecure: bool = True
+        ssl_credentials: grpc.ChannelCredentials | None = None
+
+        if self.__settings.otel_exporter_certificate is not None:
+            self._insecure = False
+            ssl_credentials = grpc.ssl_channel_credentials(
+                root_certificates=open(self.__settings.otel_exporter_certificate, "rb").read(),
+                private_key=(
+                    open(self.__settings.otel_exporter_privkey, "rb").read()
+                    if self.__settings.otel_exporter_privkey
+                    else None
+                ),
+                certificate_chain=(
+                    open(self.__settings.otel_exporter_certchain, "rb").read()
+                    if self.__settings.otel_exporter_certchain
+                    else None
+                ),
+            )
+
+        self._resource = Resource.create(attributes=self.__settings.resource_attributes)
+
+        self._span_exporter = OTLPSpanExporter(
+            endpoint=self.__settings.otel_exporter_endpoint,
+            headers=self.__settings.otel_exporter_headers,
+            insecure=self._insecure,
+            credentials=ssl_credentials,
+        )
+        self._span_processor = BatchSpanProcessor(span_exporter=self._span_exporter)
+        self._tracer_provider = TracerProvider(resource=self._resource)
+        self._tracer_provider.add_span_processor(span_processor=self._span_processor)
+
+        self._metric_exporter = OTLPMetricExporter(
+            endpoint=self.__settings.otel_exporter_endpoint,
+            headers=self.__settings.otel_exporter_headers,
+            insecure=self._insecure,
+            credentials=ssl_credentials,
+        )
+        self._metric_reader = PeriodicExportingMetricReader(
+            exporter=self._metric_exporter,
+            export_interval_millis=self.__settings.otel_export_interval_millis,
+        )
+        self._meter_provider = MeterProvider(
+            metric_readers=[self._metric_reader], resource=self._resource
+        )
+
+        otel_trace.set_tracer_provider(tracer_provider=self._tracer_provider)
+        otel_metrics.set_meter_provider(meter_provider=self._meter_provider)
+
+        self._tracers: Dict[str, Tracer] = {}
+        self._meters: Dict[str, Meter] = {}
+
+    def get_tracer(self, *, mod_name: str, lib_version: str | None = None):
+        tracer_name = f"{mod_name}|{lib_version}" if lib_version else mod_name
+
+        if tracer_name not in self._tracers:
+            self._tracers[tracer_name] = otel_trace.get_tracer(
+                instrumenting_module_name=mod_name, instrumenting_library_version=lib_version
+            )
+
+        return self._tracers[tracer_name]
+
+    def get_meter(self, *, name: str, version: str = ""):
+        meter_name = f"{name}|{version}" if version else name
+
+        if meter_name not in self._meters:
+            self._meters[meter_name] = otel_metrics.get_meter(name=name, version=version)
+
+        return self._meters[meter_name]
